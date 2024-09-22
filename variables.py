aa1dd309becce16c90fcd9951b5df326009ea02a
Petr-max41 <krilof25@mail.ru>
2024-09-22 21:11:27 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+the_number_of_completed_dz = '12'
+number_of_hours_spent = '1.5'
+course_name = 'Python'
+time_for_one_task = float(number_of_hours_spent)/ int(the_number_of_completed_dz)
+print('Курс'':',course_name,',''всего задач'':',the_number_of_completed_dz,',''затрачено часов'':',number_of_hours_spent,',''среднее время выполнения',time_for_one_task,'часа.')
